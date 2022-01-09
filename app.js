5206100faa713c6ce7afe32f46a5bb927fcde9a3
Alexandr Pryadchenko <56647239+eternalvision@users.noreply.github.com>
2022-01-09 19:57:11 +0200

    change config

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 //! UPmX11tOezXAYjD4
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Sasha:UPmX11tOezXAYjD4@cluster0.eclt4.mongodb.net/online_shop?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
 
 mongoose
   .connect(DB_HOST)
---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_HOST =
+  "mongodb+srv://Sasha:UPmX11tOezXAYjD4@cluster0.eclt4.mongodb.net/online_shop?retryWrites=true&w=majority";
+
+module.exports = {
+  DB_HOST,
+};
