345efdd2a393dad40afb6a711dcc259ba4f3d27f
Alexandr Pryadchenko <56647239+eternalvision@users.noreply.github.com>
2022-01-09 20:31:56 +0200

    fix .env

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
