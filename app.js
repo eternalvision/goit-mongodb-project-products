8484e7bc39ecb28539db33f10f3db98c1d286d98
Alexandr Pryadchenko <56647239+eternalvision@users.noreply.github.com>
2022-01-09 20:08:33 +0200

    conn to db

---
+++ app.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const { DB_HOST } = require("./config");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connect"))
+  .catch((error) => {
+    console.log(error.message);
+
+    //! обработка ошибки подключения
+    process.exit(1);
+  });
